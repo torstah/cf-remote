f87b00f8c13b6fc79dcf507a5918bfb4b1806064
Tor Castensson <tor@plan8.se>
2025-02-07 00:28:22 +0100

    res

--- server/api/resolution.get.ts
+++ server/api/resolution.get.ts
@@ -1,23 +1,49 @@
-import { defineEventHandler } from 'h3'
+import { defineEventHandler, createError } from 'h3'
+
+interface UE5Response {
+  Location?: {
+    X: number
+    Y: number
+    Z: number
+  }
+  Resolution?: {
+    X: number
+    Y: number
+  }
+  // ... other properties
+}
 
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig()
   
   try {
-    // Get the game user settings object which contains resolution info
-    const response = await $fetch(`${config.ueServer}/remote/object/property`, {
-      method: 'GET',
+    const response = await $fetch<UE5Response>(`${config.ueServer}/remote/object/property`, {
+      method: 'PUT',
       headers: {
         'Content-Type': 'application/json'
       },
-      query: {
+      body: {
         objectPath: '/Game/Maps/_MasterMain._MasterMain:PersistentLevel.BP_RemoteIntermediate_C_1',
-        propertyName: 'CurrentResolution'  // You might need to adjust this property name
+        access: 'READ_ACCESS'
       }
     })
     
-    console.log('Resolution response:', response)
-    return response
+    console.log('Full UE5 response:', response)
+
+    // Check if we have resolution info in the response
+    if (response.Resolution) {
+      return {
+        x: response.Resolution.X,
+        y: response.Resolution.Y
+      }
+    }
+
+    // If we can't find resolution info, return default values
+    console.warn('Resolution info not found in response, using defaults')
+    return {
+      x: 1920,
+      y: 1080
+    }
 
   } catch (error) {
     console.error('Failed to get resolution:', error)
