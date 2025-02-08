b3fd02cd26690714fe6a89ef7056e45b53ccf3aa
Tor Castensson <tor@plan8.se>
2025-02-08 16:04:46 +0100

    start

---
+++ start-game.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Starting Connected Futures
+
+:: Start the game with required parameters
+start "" "C:\Users\Plan8-PC\Desktop\ProdBuild\Windows\CF_2025.exe" -RCWebControlEnable -RCWebInterfaceEnable
+
